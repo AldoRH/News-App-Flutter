438656cdbeec4eeda88b802ddfdce8650fb4e45f
AldoRH <aldo.romeroh@gmail.com>
2024-02-21 16:02:05 -0600

    Create tabs_screen
    Pantalla que muestra noticias generales y muestra un circulo de carga mientras cargan las noticias

---
+++ lib/screens/tab1_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'package:news_app/services/news_service.dart';
+import 'package:news_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
+
+class Tab1Screen extends StatefulWidget {
+  const Tab1Screen({super.key});
+
+  @override
+  State<Tab1Screen> createState() => _Tab1ScreenState();
+}
+
+class _Tab1ScreenState extends State<Tab1Screen> with AutomaticKeepAliveClientMixin {
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+
+    final newsService = Provider.of<NewsService>(context);
+    
+    return Scaffold(
+      
+      body: (newsService.headlines.isEmpty)
+            ? const Center(child: CircularProgressIndicator(),)
+            : ListNews(noticias: newsService.headlines,)
+
+    );
+  }
+  
+  @override
+  bool get wantKeepAlive => true;
+}
